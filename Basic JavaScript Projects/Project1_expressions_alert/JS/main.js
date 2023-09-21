d9ac413a6c776d47541db77f3d1c161d63fdf671
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-09-20 18:29:40 -0700

    Basic Java Lessons

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,39 @@
+/*
+
+alert("Hello, World1!");
+window.alert("Hello, world2!");
+document.write('Hello, world3!');
+
+var A ="This is a string";
+document.write(A);
+
+var B ="This is a string assignment";
+window.alert(B);
+
+document.write("List told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\"Bart responded.");
+
+document.write("\"Be who you are and say what you feel,"
++ "because those who mind don\'t matter and those who matter don'\t mind.\""
++ "-Dr. Seuss");
+
+var B = "Concatenated" + "String" //concatenated string variable
+document.write(B);
+
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermione";
+document.write(Dad);
+
+
+document.write(3+3);
+
+*/
+
+
+
+var Sent1 ="This is the beginning of the string"; //set my variables 1
+var Sent2 =" and this is the end of the string."; //set my variables 2
+
+alert(Sent1); //alert of var 1
+window.alert(Sent2); //alert of var 2
+
+var concatenated = Sent1 + Sent2; //expression and concatenation
+document.write(concatenated); 
