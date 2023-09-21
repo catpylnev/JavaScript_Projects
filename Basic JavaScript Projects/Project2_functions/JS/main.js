7af2d4199aa1840b196dfa4bd86a37acac76e517
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-09-20 21:01:28 -0700

    Basic Functions2

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,17 @@
+
+/*function fun_math(){
+    var x = 4;
+    var y = 6;
+    var c = x + y;
+
+    var str = "x + y = ";
+    document.getElementById("Button").innerHTML = str + c;
+}
+Test funtion for challenge*/ 
+
+
+function myFunction() { 
+    var sentence = "I am learning"; // Create a variable and assign it string
+    sentence += " a lot from this course!"; // Concatenate the string to sentence variable.
+    document.getElementById("Concatenate").innerHTML = sentence; //set innterHTML to new sentence variable
+}
