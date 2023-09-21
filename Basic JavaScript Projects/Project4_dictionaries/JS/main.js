ccc48e364a0022872ca0ffec9118a845087b5251
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-09-21 15:59:24 -0700

    Project4 Dictionaries

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,11 @@
+function my_Dictionary(){ //function to call my dictionary
+    var Animal = { //created a dictionary
+        Species:"Cat",
+        Color:"Ginger",
+        Breed:"American Shorthair",
+        Age:5,
+        Sound:"Meow!"
+    };
+    delete Animal.Species; //deleted one of the entries in the dictionary
+    document.getElementById("Dictionary").innerHTML = Animal.Breed; //displays the entry that it calls for
+}
