8cae7c323e1d4bde55eb612715e4dd210821d78c
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-09-23 14:12:00 -0700

    Project 7 Functions

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -1,19 +1,46 @@
-/*var X = 10;
 
+//ASSIGN A LOCAL VARIABLE AND A GLOBAL VARIABLE 
 function Add_numbers_1(){
-    console.log(15 + X);
+    var X = 10;  
+    var Y = 15
+    console.log(Y + X);
 }
 
 function Add_numbers_2() {
-    console.log(X + 100);
+    console.log(X + 100); // Console shows error because x is not global fixed by putting global x var
 }
 
 Add_numbers_1();
 Add_numbers_2();
-*/
 
-function get_Date(){
-    if (new Date().getHours() < 18){
-        document.getElementById("Greeting").innerHTML = "How are you today?";
+
+
+// my if  function requirment 1 gets user input and displays message based on time
+function get_Date() {
+    var userInput = parseInt(document.getElementById("UserInput").value); //retrieves user input
+
+    if (userInput < 0 || userInput > 23) { //asks for a valid time
+        document.getElementById("Greeting").innerHTML = "Please enter a valid time (0-23).";
+    } else if (userInput < 12) { //if the morning, ask about morning
+        document.getElementById("Greeting").innerHTML = "How was your morning?";
+    } else { // displays greeting for rest of the day
+        document.getElementById("Greeting").innerHTML = "How was your day?";
     }
+}
+
+
+//Time_function as outlined
+function Time_function(){ 
+    var Time = new Date().getHours(); //get current hour of the day
+    var Reply;
+    if (Time<12 == Time > 0){ //checks if its morning
+        Reply= "It is morning time!";
+    }
+    else if(Time >= 12 == Time < 18){ //checks if its afternoon
+        Reply = "It is afternoon.";
+    }
+    else{ //assumes it is evening if not morning or afternoon
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply; //displays the determined time of day message.
 }
