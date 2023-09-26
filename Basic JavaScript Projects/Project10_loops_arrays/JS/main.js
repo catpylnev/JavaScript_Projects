0376e02ffbf9d17840976b6ee1f8e6ced0fa1c42
Catherine Pylnev <144393802+catpylnev@users.noreply.github.com>
2023-09-26 06:14:45 -0700

    Project 10 Finished

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,28 +1,28 @@
-function count_To_Ten(){
+function count_To_Ten(){ // while loop counting function
     var Digit = "";
-    var X = 1;
+    var X = 1; //starts at 1
     while (X<11) {
-        Digit +="<br>" + X;
-        X++;
+        Digit +="<br>" + X; // while under 11 (counting to 10)
+        X++; // add 1 every loop
     }
     document.getElementById("Counting_to_Ten").innerHTML = Digit;
 }
 
-let text = "Hello World!";
-let length = text.length;
-console.log(length);
+let text = "Hello World!"; 
+let length = text.length; //find length of string 
+console.log(length);  //console log
 
-var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"]; //array of isntruments
 var Content = "";
 var Y;
 function for_Loop() {
     for (Y = 0; Y < Instruments.length; Y++) {
-        Content += Instruments[Y] + "<br>";
+        Content += Instruments[Y] + "<br>"; //displays all instruments in array by placement
     }
     document.getElementById("List_of_Instruments").innerHTML = Content;
 }
 
-function cat_pics(){
+function cat_pics(){  //function that describes an activity in html
     var Cat_Picture = [];
     Cat_Picture[0] = "sleeping";
     Cat_Picture[1] = "playing";
@@ -31,9 +31,35 @@ function cat_pics(){
     document.getElementById("Cat").innerHTML = "In this picture the cat is " + Cat_Picture[2] + ".";
 }
 
-function constant_function(){
-    const Musical_Instrument = {type:"guitar", brand:"Fender", color:"black"};
-    Musical_Instrument.color = "blue";
+function constant_function(){ //function using constant 
+    const Musical_Instrument = {type:"guitar", brand:"Fender", color:"black"}; //constant
+    Musical_Instrument.color = "blue"; //editing const
     Musical_Instrument.price = "$900";
-    document.getElementById("Cosntant").innerHTML = "The cost of the " +
+    document.getElementById("Constant").innerHTML = "The cost of the " + Musical_Instrument.type + " was " + Musical_Instrument.price;
 }
+
+var X = 82;
+document.write(X);
+{
+    let X = 33; 
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
+
+
+var R = returnFunction(5,3); //function to return two numbers
+ function returnFunction(a, b) {
+    return a * b; //using the return command
+ }
+ console.log(R); //result in console log
+
+ let Car = { //let for description function
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "red ",
+    description: function() {
+        return "The car is a " + this.year + this.color + this.make + this.model; //function returns characteristics.
+    }
+ };
+ document.getElementById("Car_Object").innerHTML = Car.description();
